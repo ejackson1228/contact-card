c72d90386ca69263913bf7b23de4dbb43b391635
ejackson1228 <ejackson1228@gmail.com>
2022-11-17 13:59:20 -0500

    added edit functionaity to cards

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,7 +12,7 @@ import Dog from "../images/dog.png";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { initdb, getdb, postdb, deletedb } from "./database";
+import { initdb, getdb, postdb, deletedb, editdb } from "./database";
 
 import { fetchCards } from "./cards";
 
@@ -53,6 +53,14 @@ form.addEventListener("submit", (event) => {
   if (submitBtnToUpdate == false) {
     postdb(name, email, phone, profile);
   } else {
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editdb(profileId, name, email, phone, profile);
+
     fetchCards();
     // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -73,4 +81,23 @@ window.deleteCard = (e) => {
     deletedb(id);
     //reload the DOM
     fetchCards();
+};
+
+window.editCard = (e) => {
+    // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+    profileId = parseInt(e.dataset.id);
+  
+    // Grabs information to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+  
+    document.getElementById("name").value = editName;
+    document.getElementById("email").value = editEmail;
+    document.getElementById("phone").value = editPhone;
+  
+    form.style.display = "block";
+  
+    // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
 };
